847b03ff0ddac23b6da06a35c45f36e9efa349da
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-09 19:28:50 +0300

    User Sınıfımızın Mapping İşlemlerini Oluşturalım

--- Blogesque.Data/Concrete/EntityFramework/Contexts/ProgrammersBlogContext.cs
+++ Blogesque.Data/Concrete/EntityFramework/Contexts/ProgrammersBlogContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Blogesque.Data.Concrete.EntityFramework.Mappings;
 using Blogesque.Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
 
@@ -20,6 +21,15 @@ namespace Blogesque.Data.Concrete.EntityFramework.Contexts
         {
             optionsBuilder.UseSqlServer(
                 @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Blogesque;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ArticleMap());
+            modelBuilder.ApplyConfiguration(new CategoryMap());
+            modelBuilder.ApplyConfiguration(new CommentMap());
+            modelBuilder.ApplyConfiguration(new RoleMap());
+            modelBuilder.ApplyConfiguration(new UserMap());
         }
     }
 }
