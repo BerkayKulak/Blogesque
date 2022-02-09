23fb4ecba0df0147d7dea996700291dffe116972
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-09 18:40:44 +0300

    EfEntityRepositoryBase'den Türeyen Repository Sınıflarımızın Oluşturulması

---
+++ Blogesque.Data/Concrete/CommentRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blogesque.Data.Abstract;
+using Blogesque.Entities.Concrete;
+using Blogesque.Shared.Data.Concrete.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blogesque.Data.Concrete
+{
+    public class CommentRepository : EfEntityRepositoryBase<Comment>, ICommentRepository
+    {
+        public CommentRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
