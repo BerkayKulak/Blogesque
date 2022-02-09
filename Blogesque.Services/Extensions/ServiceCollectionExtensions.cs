f0b42aabea049fd776fce6c4fdc690ea4a3fc27a
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-10 00:24:00 +0300

    ServiceCollectionExtensions Sınıfımızı ve LoadMyServices Metodumuzu Oluşturalım

---
+++ Blogesque.Services/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blogesque.Data.Abstract;
+using Blogesque.Data.Concrete;
+using Blogesque.Data.Concrete.EntityFramework.Contexts;
+using Blogesque.Services.Abstract;
+using Blogesque.Services.Concrete;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Blogesque.Services.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection LoadMyServices(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddDbContext<ProgrammersBlogContext>();
+            serviceCollection.AddScoped<IUnitOfWork, UnitOfWork>();
+            serviceCollection.AddScoped<ICategoryService, CategoryManager>();
+            serviceCollection.AddScoped<IArticleService, ArticleManager>();
+            return serviceCollection;
+        }
+    }
+}
