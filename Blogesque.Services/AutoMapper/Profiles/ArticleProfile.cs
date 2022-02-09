94e56f41133d912896699b12d9325c10c03e86b9
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-09 23:54:43 +0300

    AutoMappper Kütüphanesini Projemize Ekleyelim ve Entegre Edelim

---
+++ Blogesque.Services/AutoMapper/Profiles/ArticleProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Blogesque.Entities.Concrete;
+using Blogesque.Entities.Dtos;
+
+namespace Blogesque.Services.AutoMapper.Profiles
+{
+    public class ArticleProfile : Profile
+    {
+        public ArticleProfile()
+        {
+            CreateMap<ArticleAddDto, Article>().ForMember(dest => dest.CreatedDate, opt => opt.MapFrom(x => DateTime.Now));
+            CreateMap<ArticleUpdateDto, Article>().ForMember(dest => dest.ModifiedDate, opt => opt.MapFrom(x => DateTime.Now));
+        }
+    }
+}
---
+++ Blogesque.Services/AutoMapper/Profiles/CategoryProfile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Blogesque.Entities.Concrete;
+using Blogesque.Entities.Dtos;
+
+namespace Blogesque.Services.AutoMapper.Profiles
+{
+    public class CategoryProfile : Profile
+    {
+        public CategoryProfile()
+        {
+            CreateMap<CategoryAddDto, Category>()
+                .ForMember(dest => dest.CreatedDate, opt => opt.MapFrom(x => DateTime.Now));
+            CreateMap<CategoryUpdateDto, Category>()
+                .ForMember(dest => dest.ModifiedDate, opt => opt.MapFrom(x => DateTime.Now));
+        }
+    }
+}
