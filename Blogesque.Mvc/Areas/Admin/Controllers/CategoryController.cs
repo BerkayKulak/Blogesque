d8fcc1e7522b1d76517f1247807db508c52d471e
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-10 01:50:03 +0300

    Category Controller, Index Action ve Index View'ini Oluşturalım

---
+++ Blogesque.Mvc/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Blogesque.Services.Abstract;
+using Blogesque.Shared.Utilities.Results.ComplexTypes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blogesque.Mvc.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var result  = await _categoryService.GetAll();
+
+            if (result.ResultStatus == ResultStatus.Success)
+            {
+                return View(result.Data);
+            }
+
+            return View();
+        }
+    }
+}
