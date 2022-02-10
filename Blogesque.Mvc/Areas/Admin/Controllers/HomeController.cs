0e5cdd47853dbf6642d12113f8fc36291a4559bb
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-10 23:08:40 +0300

    DashboardViewModel Sınıfımızı ve Home / Index Action'ımızı Oluşturalım

--- Blogesque.Mvc/Areas/Admin/Controllers/HomeController.cs
+++ Blogesque.Mvc/Areas/Admin/Controllers/HomeController.cs
@@ -2,8 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blogesque.Entities.Concrete;
+using Blogesque.Mvc.Areas.Admin.Models;
+using Blogesque.Services.Abstract;
+using Blogesque.Shared.Utilities.Results.ComplexTypes;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Blogesque.Mvc.Areas.Admin.Controllers
 {
@@ -11,9 +17,40 @@ namespace Blogesque.Mvc.Areas.Admin.Controllers
     [Authorize(Roles = "Admin,Editor")]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ICategoryService _categoryService;
+        private readonly IArticleService _articleService;
+        private readonly ICommentService _commentService;
+        private readonly UserManager<User> _userManager;
+
+        public HomeController(ICategoryService categoryService, IArticleService articleService, ICommentService commentService, UserManager<User> userManager)
         {
-            return View();
+            _categoryService = categoryService;
+            _articleService = articleService;
+            _commentService = commentService;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var categoriesCountResult = await _categoryService.CountByIsDeleted();
+            var articlesCountResult = await _articleService.CountByIsDeleted();
+            var commentsCountResult = await _commentService.CountByIsDeleted();
+            var usersCount = await _userManager.Users.CountAsync();
+            var articlesResult = await _articleService.GetAll();
+            if (categoriesCountResult.ResultStatus == ResultStatus.Success && articlesCountResult.ResultStatus == ResultStatus.Success && commentsCountResult.ResultStatus == ResultStatus.Success && usersCount > -1 && articlesResult.ResultStatus == ResultStatus.Success)
+            {
+                return View(new DashboardViewModel
+                {
+                    CategoriesCount = categoriesCountResult.Data,
+                    ArticlesCount = articlesCountResult.Data,
+                    CommentsCount = commentsCountResult.Data,
+                    UsersCount = usersCount,
+                    Articles = articlesResult.Data
+                });
+            }
+
+            return NotFound();
+
         }
     }
 }
