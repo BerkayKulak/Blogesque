8f1f814967852320512f9090a7d2e103154f70ac
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-10 21:48:42 +0300

    ImageDeletedDto Sınıfımızı ve Delete() Metodumuzu Oluşturalım

--- Blogesque.Mvc/Helpers/Abstract/IImageHelper.cs
+++ Blogesque.Mvc/Helpers/Abstract/IImageHelper.cs
@@ -8,5 +8,6 @@ namespace Blogesque.Mvc.Helpers.Abstract
     public interface IImageHelper
     {
         Task<IDataResult<UploadedImageDto>> UploadUserImage(string userName, IFormFile pictureFile, string folderName = "userImages");
+        IDataResult<ImageDeletedDto> Delete(string pictureName);
     }
 }
