d3bff957814661bfcebbe5c3b22dafabb16c6dd0
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-10 09:39:59 +0300

    Ajax POST İşlemleri için Hazırlıklarımızı Tamamlayalım

---
+++ Blogesque.Mvc/Areas/Admin/Models/CategoryAddAjaxViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blogesque.Entities.Dtos;
+
+
+namespace Blogesque.Mvc.Areas.Admin.Models
+{
+    public class CategoryAddAjaxViewModel
+    {
+        public CategoryAddDto CategoryAddDto { get; set; }
+        public string CategoryAddPartial { get; set; }
+        public CategoryDto CategoryDto { get; set; }
+    }
+}
