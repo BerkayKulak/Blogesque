45a9ec942208da130a12c2d6adca4024a748c7c4
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-10 15:12:47 +0300

    Migration ve Veritabanı Oluşturma İşlemlerimizi Tamamlayalım

--- Blogesque.Services/Extensions/ServiceCollectionExtensions.cs
+++ Blogesque.Services/Extensions/ServiceCollectionExtensions.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Blogesque.Data.Abstract;
 using Blogesque.Data.Concrete;
 using Blogesque.Data.Concrete.EntityFramework.Contexts;
+using Blogesque.Entities.Concrete;
 using Blogesque.Services.Abstract;
 using Blogesque.Services.Concrete;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,6 +18,19 @@ namespace Blogesque.Services.Extensions
         public static IServiceCollection LoadMyServices(this IServiceCollection serviceCollection)
         {
             serviceCollection.AddDbContext<ProgrammersBlogContext>();
+            serviceCollection.AddIdentity<User, Role>(options =>
+            {
+                // User Password Options
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 5;
+                options.Password.RequiredUniqueChars = 0;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                // User Username and Email Options
+                options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+$";
+                options.User.RequireUniqueEmail = true;
+            }).AddEntityFrameworkStores<ProgrammersBlogContext>();
             serviceCollection.AddScoped<IUnitOfWork, UnitOfWork>();
             serviceCollection.AddScoped<ICategoryService, CategoryManager>();
             serviceCollection.AddScoped<IArticleService, ArticleManager>();
