d586e51b9f8559e544fe0aa81e6a6810e01c4ec1
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-10 19:59:26 +0300

    Admin Menümüzü Dinamik Bir Hale Getirmek için ViewComponent Olarak Revize Edelim

---
+++ Blogesque.Mvc/Areas/Admin/Models/UserWithRolesViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Blogesque.Entities.Concrete;
+
+namespace Blogesque.Mvc.Areas.Admin.Models
+{
+    public class UserWithRolesViewModel
+    {
+        public User User { get; set; }
+        public IList<string> Roles { get; set; }
+    }
+}
