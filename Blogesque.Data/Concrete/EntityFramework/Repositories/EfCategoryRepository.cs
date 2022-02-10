0d332e269350501a75e6b2774ad6dfe9b8d4af07
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-10 22:08:17 +0300

    EfEntityRepositoryBase Sınıfımız ile İlgili Küçük Bir Düzenleme Yapalım

--- Blogesque.Data/Concrete/EntityFramework/Repositories/EfCategoryRepository.cs
+++ Blogesque.Data/Concrete/EntityFramework/Repositories/EfCategoryRepository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Blogesque.Data.Abstract;
+using Blogesque.Data.Concrete.EntityFramework.Contexts;
 using Blogesque.Entities.Concrete;
 using Blogesque.Shared.Data.Concrete.EntityFramework;
 using Microsoft.EntityFrameworkCore;
@@ -14,6 +15,19 @@ namespace Blogesque.Data.Concrete.EntityFramework.Repositories
     {
         public EfCategoryRepository(DbContext context) : base(context)
         {
+        }
+
+        public async Task<Category> GetById(int categoryId)
+        {
+            return await ProgrammersBlogContext.Categories.SingleOrDefaultAsync(c => c.Id == categoryId);
+        }
+
+        private ProgrammersBlogContext ProgrammersBlogContext
+        {
+            get
+            {
+                return _context as ProgrammersBlogContext;
+            }
         }
     }
 }
