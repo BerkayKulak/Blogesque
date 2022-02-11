75e6e8a61fbe438381336692ae894e44d1929694
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-11 21:47:12 +0300

    EfEntityRepositoryBase için GetAsyncV2 & GetAllAsyncV2 Metodları Oluşturuldu

--- Blogesque.Shared/Data/Abstract/IEntityRepository.cs
+++ Blogesque.Shared/Data/Abstract/IEntityRepository.cs
@@ -11,9 +11,12 @@ namespace Blogesque.Shared.Data.Abstract
     public interface IEntityRepository<T> where T : class, IEntity, new()
     {
         Task<T> GetAsync(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties); // var kullanici = repository.GetAsync(k=>k.Id==15);
+        Task<T> GetAsyncV2(IList<Expression<Func<T, bool>>> predicates, IList<Expression<Func<T, object>>> includeProperties);
+        // if(isActive==true) predicates.Add()
 
         Task<IList<T>> GetAllAsync(Expression<Func<T, bool>> predicate = null,
             params Expression<Func<T, object>>[] includeProperties);
+        Task<IList<T>> GetAllAsyncV2(IList<Expression<Func<T, bool>>> predicates, IList<Expression<Func<T, object>>> includeProperties);
 
         Task<T> AddAsync(T entity);
         Task<T> UpdateAsync(T entity);
