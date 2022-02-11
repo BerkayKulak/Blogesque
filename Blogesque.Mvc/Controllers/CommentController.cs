39ccee5adcb82c4cc906c7e85c88cdde8b24dd94
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-11 15:31:08 +0300

    Yorum Ekleme İşlemlerimizin Backend Kısımlarını Oluşturalım

---
+++ Blogesque.Mvc/Controllers/CommentController.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Blogesque.Entities.Dtos;
+using Blogesque.Mvc.Models;
+using Blogesque.Services.Abstract;
+using Blogesque.Shared.Utilities.Extensions;
+using Blogesque.Shared.Utilities.Results.ComplexTypes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blogesque.Mvc.Controllers
+{
+    public class CommentController : Controller
+    {
+        private readonly ICommentService _commentService;
+
+        public CommentController(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+        [HttpPost]
+        public async Task<JsonResult> Add(CommentAddDto commentAddDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _commentService.AddAsync(commentAddDto);
+                if (result.ResultStatus == ResultStatus.Success)
+                {
+                    var commentAddAjaxViewModel = JsonSerializer.Serialize(new CommentAddAjaxViewModel
+                    {
+                        CommentDto = result.Data,
+                        CommentAddPartial = await this.RenderViewToStringAsync("_CommentAddPartial", commentAddDto)
+                    }, new JsonSerializerOptions
+                    {
+                        ReferenceHandler = ReferenceHandler.Preserve
+                    });
+                    return Json(commentAddAjaxViewModel);
+                }
+                ModelState.AddModelError("", result.Message);
+            }
+            var commentAddAjaxErrorModel = JsonSerializer.Serialize(new CommentAddAjaxViewModel
+            {
+                CommentAddDto = commentAddDto,
+                CommentAddPartial = await this.RenderViewToStringAsync("_CommentAddPartial", commentAddDto)
+            });
+            return Json(commentAddAjaxErrorModel);
+        }
+
+    }
+}
