fdbc9538b2a51ef5d2095be327f3f8e8ac896f0c
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-11 12:27:55 +0300

    Yeni Rol Yönetim Sistemimi İmplemente Edildi

--- Blogesque.Mvc/Areas/Admin/Controllers/HomeController.cs
+++ Blogesque.Mvc/Areas/Admin/Controllers/HomeController.cs
@@ -14,7 +14,6 @@ using Microsoft.EntityFrameworkCore;
 namespace Blogesque.Mvc.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    [Authorize(Roles = "Admin,Editor")]
     public class HomeController : Controller
     {
         private readonly ICategoryService _categoryService;
@@ -29,7 +28,7 @@ namespace Blogesque.Mvc.Areas.Admin.Controllers
             _commentService = commentService;
             _userManager = userManager;
         }
-
+        [Authorize(Roles = "SuperAdmin,AdminArea.Home.Read")]
         public async Task<IActionResult> Index()
         {
             var categoriesCountResult = await _categoryService.CountByNonDeletedAsync();
--- Blogesque.Mvc/Startup.cs
+++ Blogesque.Mvc/Startup.cs
@@ -35,8 +35,8 @@ namespace Blogesque.Mvc
             services.AddScoped<IImageHelper, ImageHelper>();
             services.ConfigureApplicationCookie(options =>
             {
-                options.LoginPath = new PathString("/Admin/User/Login");
-                options.LogoutPath = new PathString("/Admin/User/Logout");
+                options.LoginPath = new PathString("/Admin/Auth/Login");
+                options.LogoutPath = new PathString("/Admin/Auth/Logout");
                 options.Cookie = new CookieBuilder
                 {
                     Name = "ProgrammersBlog",
@@ -46,7 +46,7 @@ namespace Blogesque.Mvc
                 };
                 options.SlidingExpiration = true;
                 options.ExpireTimeSpan = System.TimeSpan.FromDays(7);
-                options.AccessDeniedPath = new PathString("/Admin/User/AccessDenied");
+                options.AccessDeniedPath = new PathString("/Admin/Auth/AccessDenied");
             });
         }
 
