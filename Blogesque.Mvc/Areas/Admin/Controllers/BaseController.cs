49e5413f5598ead07f185a3b881adef0c544a5a7
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-11 10:26:50 +0300

    BaseController Sınıfımızı Oluşturalım

---
+++ Blogesque.Mvc/Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Blogesque.Entities.Concrete;
+using Blogesque.Mvc.Helpers.Abstract;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blogesque.Mvc.Areas.Admin.Controllers
+{
+    public class BaseController : Controller
+    {
+        public BaseController(UserManager<User> userManager, IMapper mapper, IImageHelper imageHelper)
+        {
+            UserManager = userManager;
+            Mapper = mapper;
+            ImageHelper = imageHelper;
+        }
+
+        protected UserManager<User> UserManager { get; }
+        protected IMapper Mapper { get; }
+        protected IImageHelper ImageHelper { get; }
+        protected User LoggedInUser => UserManager.GetUserAsync(HttpContext.User).Result;
+    }
+}
